e76c36afa944cfedbc97ce0595a576c1aa2ffeda
Oleg Vodyanov <oleg.vodyanov91@gmail.com>
2022-11-24 23:16:18 +0400

    Added calculation logic of two numbers in Main method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int sum = firstNumber + secondNumber;
+        int subtraction = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        double division = (double) firstNumber / (double) secondNumber;
+
+
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + subtraction);
+        System.out.println("Произведение: " + multiplication);
+        System.out.println("Частное: " + division);
     }
 }
