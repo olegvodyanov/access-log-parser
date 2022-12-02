e5a0905fbe32fef9b87be0fddaf2c8b501d2cf4f
Oleg Vodyanov <oleg.vodyanov91@gmail.com>
2022-12-03 00:18:56 +0400

    Изменил метод main. Добавил бесконечный цикл для проверки наличия файла на жестком диске по введенному пути. Ведется подсчет корректно введенных путей к файлу.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int counterOfCorrectlyProvidedFiles = 0;
+        while (true) {
+            System.out.println("Пожалуйста, ведите путь к файлу ниже.");
+
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists) {
+                System.out.println("Файл не существует, попробуйте, пожалуйста, ещё раз.");
+                continue;
+            } else if (isDirectory) {
+                System.out.println("Это директория, попробуйте, пожалуйста, ещё раз.");
+                continue;
+            } else {
+                System.out.println("Путь к файлу указан верно.");
+                counterOfCorrectlyProvidedFiles++;
+                System.out.println("Количество верно указанных файлов равно: " + counterOfCorrectlyProvidedFiles);
+            }
+        }
     }
 }
