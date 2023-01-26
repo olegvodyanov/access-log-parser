5443ecff02480f9f643db3c59528ade1bb2dbdb8
Oleg Vodyanov <oleg.vodyanov91@gmail.com>
2023-01-27 00:16:24 +0400

    Добавил несколько методов и переменных для расчета доли используемых операционных систем, а так для составления списка уникальных адресов сайта из файла лога. Все изменения произведены в классе Statistics.java

--- src/Statistics.java
+++ src/Statistics.java
@@ -4,16 +4,21 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.*;
 
 public class Statistics {
     private long totalTraffic;
     private LocalDateTime minTime;
     private LocalDateTime maxTime;
+    private HashSet<String> allEndpoints;
+    private HashMap<String, Integer> operatingSystemStat;
 
     public Statistics() {
         totalTraffic = 0;
         minTime = null;
         maxTime = null;
+        allEndpoints = new HashSet<>();
+        operatingSystemStat = new HashMap<>();
     }
 
     public void addEntry(LogEntry logEntry) {
@@ -22,6 +27,19 @@ public class Statistics {
             minTime = logEntry.getTime();
         } else if (maxTime == null || logEntry.getTime().isAfter(maxTime)) {
             maxTime = logEntry.getTime();
+        }
+
+        String logEntryPath = logEntry.getPath();
+        if (logEntry.getResponseCode() == 200) {
+            allEndpoints.add(logEntryPath);
+        }
+
+        String logEntryOsType = logEntry.getAgent().getTypeOS();
+        if (operatingSystemStat.containsKey(logEntryOsType)) {
+            int counter = operatingSystemStat.get(logEntryOsType);
+            operatingSystemStat.replace(logEntryOsType, ++counter);
+        } else {
+            operatingSystemStat.put(logEntryOsType, 1);
         }
     }
 
@@ -33,6 +51,28 @@ public class Statistics {
         double durationInHours = hours + minutesToHours + secondsToHours;
 
         return BigDecimal.valueOf(totalTraffic).divide(BigDecimal.valueOf(durationInHours), RoundingMode.HALF_UP).longValue();
+    }
+
+    public List<String> getAllUniquePagesExistingInLogFile() {
+        return new ArrayList<>(allEndpoints);
+    }
+
+    public HashMap<String, Double> getOsTypeProportionStat() {
+        HashMap<String, Double> typeOsProportionMap = new HashMap<>();
+        int totalOsCounter = totalOsTypesCalculator();
+
+        for (Map.Entry<String, Integer> set : operatingSystemStat.entrySet()) {
+            typeOsProportionMap.put(set.getKey(), ((double) set.getValue() / (double) totalOsCounter) );
+        }
+        return typeOsProportionMap;
+    }
+
+    private int totalOsTypesCalculator() {
+        int typeOsCounter = 0;
+        for (Map.Entry<String, Integer> set : operatingSystemStat.entrySet()) {
+            typeOsCounter += set.getValue();
+        }
+        return typeOsCounter;
     }
 
     public long getTotalTraffic() {
